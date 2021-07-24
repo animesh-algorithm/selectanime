deaebe046f26c7b4417894953b74b8ee3617dfc0
Animesh Sharma <animesharma3@gmail.com>
2021-07-24 23:07:58 +0530

    Fixing CORS error

--- server/settings.py
+++ server/settings.py
@@ -125,6 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000', 'http://localhost:8000'
+]
+
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'client/build/static')
